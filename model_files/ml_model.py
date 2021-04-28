9118c146002cfc015f002e38b5d97d22b1bec97a
rishabh201219 <70973457+rishabh201219@users.noreply.github.com>
2021-04-28 20:11:50 +0530

    Update ml_model.py

--- model_files/ml_model.py
+++ model_files/ml_model.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import seaborn as sns
-import matplotlib.pyplot as plt 
+#import matplotlib.pyplot as plt 
 
 
 from sklearn.model_selection import StratifiedShuffleSplit
